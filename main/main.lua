0e5058cb59b8e3ac1188ae74e1249ed805dc49bd
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-11-24 17:59:44 +0200

    'update'

--- main/main.lua
+++ main/main.lua
@@ -1,13 +1,23 @@
 local Str = require("./test/temp")
 
 function love.load()
-    world = love.physics.newWorld(0,0,true)
+    World = love.physics.newWorld(0,0,true)
+    Entity = {}
+    Entity.body= love.physics.newBody(World,300,300,'static')
+    Entity.shape1= love.physics.newRectangleShape(47,47)
+    Entity.shape2= love.physics.newCircleShape(300,300,47)
+    Entity.fixture1= love.physics.newFixture(Entity.body,Entity.shape1,1)
+    Entity.fixture2= love.physics.newFixture(Entity.body,Entity.shape2,1)
+
     
 end
-function love.update()
-    
+function love.update(dt)
+    World:update(dt)
 end
 
 function love.draw()
-    love.graphics.print(Str.string, 400, 300)
+    love.graphics.polygon("line", Entity.body:getWorldPoints(Entity.shape1:getPoints()))
+    love.graphics.circle("line",Entity.body.shape2:getX(),Entity.body.shape2:getY(),Entity.body.shape2:getRadius())
+    -- love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+
 end
