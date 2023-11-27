2197796b1c83a23d7f5d0837de1b0e81b768b98c
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-11-27 15:34:43 +0200

    'update'

--- main/main.lua
+++ main/main.lua
@@ -1,7 +1,20 @@
 local Str = require("./test/temp")
+local particleImage = love.graphics.newImage("assets/green.png")
+
+-- Create a particle system
+local particleSystem = love.graphics.newParticleSystem(particleImage, 1000)
+particleSystem:setParticleLifetime(1, 2)
+particleSystem:setLinearAcceleration(-20, -20, 20, 20)
+particleSystem:setColors(0, 1, 0, 1, 0, 1, 0, 0)  -- Green color
+particleSystem:setSizes(2, 1, 0.5)  -- Varying sizes for the particles
+
 
 function love.load()
-    World = love.physics.newWorld(0, 0, true)
+    World = love.physics.newWorld(0, 10, true)
+    Border = {}
+    Border.body =love.physics.newBody(World,0,0,"static")
+    Border.shape= love.physics.newChainShape(true,0,0,800,0,800,600,0,600   )
+    Border.fixture= love.physics.newFixture(Border.body,Border.shape,1)
     Entity = {}
     Entity.body = love.physics.newBody(World, 300, 300, 'static')
     Entity.shape1 = love.physics.newRectangleShape(47, 47)
@@ -11,10 +24,33 @@ function love.load()
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        Entity.body:applyForce(400, 0)
+      elseif love.keyboard.isDown("left") then
+        Entity.body:applyForce(-400, 0)
+    elseif love.keyboard.isDown("up") then 
+        Entity.body:applyForce(0, 400)
+      end
+
     World:update(dt)
+
+     -- Update the particle system position
+     particleSystem:setPosition(Entity.body:getX(), Entity.body:getY())
+     particleSystem:update(dt)
+end
+
+
+function love.mousepressed(x, y, button, istouch, presses)
+    -- Emit particles when the mouse is pressed
+    particleSystem:emit(20)
 end
 
 function love.draw()
+    love.graphics.line(Border.body:getWorldPoints(Border.shape:getPoints()))
     love.graphics.polygon("line", Entity.body:getWorldPoints(Entity.shape1:getPoints()))
     love.graphics.circle("line", Entity.body:getX(), Entity.body:getY(), Entity.shape2:getRadius())
+
+
+
+    love.graphics.draw(particleSystem)
 end
