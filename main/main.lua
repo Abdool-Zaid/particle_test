e805c6a58dfdf1f103a73a76f65cbfe882a833ae
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-11-27 14:59:13 +0200

    'update'

--- main/main.lua
+++ main/main.lua
@@ -1,23 +1,20 @@
 local Str = require("./test/temp")
 
 function love.load()
-    World = love.physics.newWorld(0,0,true)
+    World = love.physics.newWorld(0, 0, true)
     Entity = {}
-    Entity.body= love.physics.newBody(World,300,300,'static')
-    Entity.shape1= love.physics.newRectangleShape(47,47)
-    Entity.shape2= love.physics.newCircleShape(300,300,47)
-    Entity.fixture1= love.physics.newFixture(Entity.body,Entity.shape1,1)
-    Entity.fixture2= love.physics.newFixture(Entity.body,Entity.shape2,1)
-
-    
+    Entity.body = love.physics.newBody(World, 300, 300, 'static')
+    Entity.shape1 = love.physics.newRectangleShape(47, 47)
+    Entity.shape2 = love.physics.newCircleShape(300, 300, 47)
+    Entity.fixture1 = love.physics.newFixture(Entity.body, Entity.shape1, 1)
+    Entity.fixture2 = love.physics.newFixture(Entity.body, Entity.shape2, 1)
 end
+
 function love.update(dt)
     World:update(dt)
 end
 
 function love.draw()
     love.graphics.polygon("line", Entity.body:getWorldPoints(Entity.shape1:getPoints()))
-    love.graphics.circle("line",Entity.shape2:getX(),Entity.shape2:getY(),Entity.shape2:getRadius())
-    -- love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
-
+    love.graphics.circle("line", Entity.body:getX(), Entity.body:getY(), Entity.shape2:getRadius())
 end
